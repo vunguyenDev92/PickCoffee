278d17945208810423b1232e214c988ef4422405
vunguyenDev92 <vunguyendev92@gmail.com>
2025-10-13 17:20:06 +0700

    fix: fix data type of price

--- app/src/main/java/com/example/androidmycoffee/domain/model/Coffee.kt
+++ app/src/main/java/com/example/androidmycoffee/domain/model/Coffee.kt
@@ -1,10 +1,12 @@
 package com.example.androidmycoffee.domain.model
 
+import java.math.BigDecimal
+
 data class Coffee(
     val id: Int,
     val name: String,
     val type: TypeCoffee,
-    val price: Int,
+    val price: BigDecimal,
 )
 
 enum class TypeCoffee {
